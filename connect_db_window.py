4ac15e1d7477f6ddf46dc3a7710d674c60eb0cbc
Woo Jia Hao <woojiahao1234@gmail.com>
2018-07-14 18:22:48 +0800

    Added some default values for the fields and checking for whether the database field is filled, if it is not filled, it will show an error message

--- connect_db_window.py
+++ connect_db_window.py
@@ -1,8 +1,12 @@
 from tkinter import *
+from tkinter import messagebox
+
 from settings import Settings
+
 
 class ConnectDbWindow:
 	"""Provides the user with the ability to connect to a database of their choice"""
+
 	def __init__(self, master: Tk):
 		self.__master = master
 		self.__config__()
@@ -17,31 +21,40 @@ class ConnectDbWindow:
 		# username
 		username = Label(master=frame, text='Username:', font=Settings.font_small)
 		username.grid(row=0, sticky=W, padx=(0, Settings.padding_x), pady=(0, Settings.padding_y))
-		self.__username_field = Entry(master=frame, font=Settings.font_small)
+		self.__username_str = StringVar()
+		self.__username_str.set('postgres')
+		self.__username_field = Entry(master=frame, textvariable=self.__username_str, font=Settings.font_small)
 		self.__username_field.grid(row=0, column=1, pady=(0, Settings.padding_y))
 
 		# password
 		password = Label(master=frame, text='Password:', font=Settings.font_small)
 		password.grid(row=1, sticky=W, padx=(0, Settings.padding_x), pady=(0, Settings.padding_y))
-		self.__password_field = Entry(master=frame, font=Settings.font_small)
+		self.__password_str = StringVar()
+		self.__password_str.set('root')
+		self.__password_field = Entry(master=frame, textvariable=self.__password_str, show='*', font=Settings.font_small)
 		self.__password_field.grid(row=1, column=1, pady=(0, Settings.padding_y))
 
 		# port
 		port = Label(master=frame, text='Port:', font=Settings.font_small)
 		port.grid(row=2, sticky=W, padx=(0, Settings.padding_x), pady=(0, Settings.padding_y))
-		self.__port_field = Entry(master=frame, font=Settings.font_small)
+		self.__port_str = StringVar()
+		self.__port_str.set('5432')
+		self.__port_field = Entry(master=frame, textvariable=self.__port_str, font=Settings.font_small)
 		self.__port_field.grid(row=2, column=1, pady=(0, Settings.padding_y))
 
 		# host
 		host = Label(master=frame, text='Host:', font=Settings.font_small)
 		host.grid(row=3, sticky=W, padx=(0, Settings.padding_x), pady=(0, Settings.padding_y))
-		self.__host_field = Entry(master=frame, font=Settings.font_small)
+		self.__host_str = StringVar()
+		self.__host_str.set('localhost')
+		self.__host_field = Entry(master=frame, textvariable=self.__host_str, font=Settings.font_small)
 		self.__host_field.grid(row=3, column=1, pady=(0, Settings.padding_y))
 
 		# database
 		database = Label(master=frame, text='Database:', font=Settings.font_small)
 		database.grid(row=4, sticky=W, padx=(0, Settings.padding_x), pady=(0, Settings.padding_y))
-		self.__database_field = Entry(master=frame, font=Settings.font_small)
+		self.__database_str = StringVar()
+		self.__database_field = Entry(master=frame, textvariable=self.__database_str, font=Settings.font_small)
 		self.__database_field.grid(row=4, column=1, pady=(0, Settings.padding_y))
 
 		# connect
@@ -56,7 +69,10 @@ class ConnectDbWindow:
 		host = self.__host_field.get()
 		database = self.__database_field.get()
 
-		
+		if database == '':
+			messagebox.showerror('No database specified', 'Please specify a database name to connect to')
+		else:
+			pass
 
 
 root = Tk()
