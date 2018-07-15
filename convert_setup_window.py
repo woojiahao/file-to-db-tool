ce4a575f0ad7ae184eb6db22558d17998af53702
Woo Jia Hao <woojiahao1234@gmail.com>
2018-07-15 08:17:23 +0800

    Added more parameters for the constructor so to take in the skip rows and delimiter

--- convert_setup_window.py
+++ convert_setup_window.py
@@ -2,10 +2,11 @@ from tkinter import *
 from db_tool import DatabaseTool
 
 class ConvertSetupWindow(Frame):
-	def __init__(self, master: Tk, db_tool: DatabaseTool, filename: str):
+	def __init__(self, master: Tk, db_tool: DatabaseTool, filename: str, skiprows: int, delimiter: str):
 		super().__init__(master=master)
 		self.__master = master
 		self.__tool = db_tool
+		self.__filename = filename
 		self.__config__()
 
 	def __config__(self):
