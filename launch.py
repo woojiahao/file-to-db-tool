7d1470781e59304a74c0237004c65ca0e1bb557f
Woo Jia Hao <woojiahao1234@gmail.com>
2018-07-15 17:39:14 +0800

    Moved code into the main file

--- launch.py
+++ launch.py
@@ -2,8 +2,7 @@ from tkinter import Tk
 
 from connect_database_window import ConnectDatabaseWindow
 
-root = Tk()
-connect_db = ConnectDatabaseWindow(root)
-
 if __name__ == '__main__':
+	root = Tk()
+	connect_db = ConnectDatabaseWindow(root)
 	root.mainloop()
