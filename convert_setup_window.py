32dd9ce065dd411cd482831d19d05dbb124b88b6
Woo Jia Hao <woojiahao1234@gmail.com>
2018-07-15 08:06:08 +0800

    modifed the parameters for the convert setup window

--- convert_setup_window.py
+++ convert_setup_window.py
@@ -2,7 +2,7 @@ from tkinter import *
 from db_tool import DatabaseTool
 
 class ConvertSetupWindow(Frame):
-	def __init__(self, master: Tk, db_tool, DatabaseTool, file_name: str):
+	def __init__(self, master: Tk, db_tool: DatabaseTool, filename: str):
 		super().__init__(master=master)
 		self.__master = master
 		self.__tool = db_tool
