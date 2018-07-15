ead108874071677ab89236963f2db6ec09a6fca1
Woo Jia Hao <woojiahao1234@gmail.com>
2018-07-15 18:47:01 +0800

    Added a utils class to remove circular dependencies

---
+++ utils.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+from file_selection_window import FileSelectionWindow
+from convert_setup_window import ConvertSetupWindow
+from db_tool import DatabaseTool
+
+def launch_file_selection(root: Tk, tool: DatabaseTool):
+	root.destroy()
+	FileSelectionWindow(Tk(), tool)
+
+def launch_setup(root: Tk, tool: DatabaseTool, filename: str, skiprows: int, delimiter: str):
+	root.destroy()
+	ConvertSetupWindow(Tk(), tool, filename, skiprows, delimiter)
