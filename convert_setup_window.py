5d657381e64cbea611a7fe6241d115ce872d2415
Woo Jia Hao <woojiahao1234@gmail.com>
2018-07-15 09:25:19 +0800

    Completed the dynamic creation of the rows

--- convert_setup_window.py
+++ convert_setup_window.py
@@ -1,11 +1,13 @@
 from tkinter import *
 import pandas as pd
+from pandas import DataFrame
 
 from db_tool import DatabaseTool
 from settings import Settings
 
 # TODO: Check if the table name is blank, if it is, warn the user to specify one otherwise the file name will be used as the table name
 # TODO: Give users the ability to change the skiprows and delimiter
+# TODO: Add checking if the intended PKs can be used as PKs
 class ConvertSetupWindow(Frame):
 	def __init__(self, master: Tk, db_tool: DatabaseTool, filename: str, skiprows: int, delimiter: str):
 		super().__init__(master=master)
@@ -16,7 +18,7 @@ class ConvertSetupWindow(Frame):
 		self.__master = master
 		self.__tool = db_tool
 
-		self.__df = self.__open_file__()
+		self.__df: DataFrame = self.__open_file__()
 		self.__config__()
 
 	def __open_file__(self):
@@ -39,11 +41,28 @@ class ConvertSetupWindow(Frame):
 		tablename.pack(side=LEFT, padx=(0, Settings.padding_x))
 		tablename_str = StringVar()
 		tablename_field = Entry(master=tablename_frame, textvariable=tablename_str, font=Settings.font_small)
-		tablename_field.pack(fill=X)
+		tablename_field.pack(fill=X, pady=(0, Settings.padding_y))
 
 		# table headers
 		self.__populate_table_headers__()
 
 	def __populate_table_headers__(self):
-		for header in self.__df.dtypes:
-			print(header)
+		for col_name, col_type in zip(self.__df.columns.values, self.__df.dtypes):
+			print('col_name: {}\ncol_type: {}\n'.format(col_name, col_type))
+
+			inline_frame = Frame(master=self)
+			inline_frame.pack(fill=X, pady=(0, Settings.padding_y))
+
+			col_name_str = StringVar()
+			col_name_str.set(col_name)
+			col_name_field = Entry(master=inline_frame, textvariable=col_name_str, font=Settings.font_small)
+			col_name_field.grid(row=0, column=0, padx=(0, Settings.padding_x))
+
+			col_type_str = StringVar()
+			col_type_str.set(col_type)
+			col_type_field = Entry(master=inline_frame, textvariable=col_type_str, font=Settings.font_small)
+			col_type_field.grid(row=0, column=1, padx=(0, Settings.padding_x))
+
+			primary_key_check = Checkbutton(master=inline_frame, text='PK', font=Settings.font_small, variable=BooleanVar())
+			primary_key_check.grid(row=0, column=2)
+
