152ff9dbd7aaab3ec07934e99585868208745ada
Woo Jia Hao <woojiahao1234@gmail.com>
2018-07-15 08:16:22 +0800

    Changed the font that is being used

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 class Settings:
 	screen_size = ('500x250')
 
-	font_small = ('Dejavu Sans Mono', 14)
-	font_medium = ('Dejavu Sans Mono', 18)
-	font_large = ('Dejavu Sans Mono', 22)
+	font_small = ('Roboto', 16)
+	font_medium = ('Roboto', 18)
+	font_large = ('Roboto', 20)
 
 	padding_x = 20
 	padding_y = 20
